08f620b2d4477a2918f472b7df9afb823ebc5203
nilesh <nileshjadhav0822@gmail.com>
2025-05-17 16:44:12 +0530

    Student Class Updated

--- src/main/java/com/rocky/model/Student.java
+++ src/main/java/com/rocky/model/Student.java
@@ -1,22 +1,18 @@
 package com.rocky.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="students")
+@Table(name="studentstable")
 public class Student {
 
 	@Id
-	@Column(name="students_id")
 	private Integer sid;
 	
-	@Column(name="city")
 	private String scity;
 	
-	@Column(name="name")
 	private String sname;
 	
 	public Student() {
